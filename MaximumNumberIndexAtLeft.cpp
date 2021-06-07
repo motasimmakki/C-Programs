efeb1aeb80afe6d31c14db99b3ba6226e17b1dfe
Motasim <44056349+motasimmakki@users.noreply.github.com>
2021-06-07 20:51:45 +0530

    Initial Commit (Using Stack Approach).
    In this program, we have been given an array of integers and for every element of an integer, we have to determine the index of the closest greater number present at the left to that number in an array.
    .
    Time Complexity: O(n), where n is the number of elements in an array.
    .
    O(n) Time Complexity is only possible because the element that is popped from the stack, that will not be pushed again.

---
+++ MaximumNumberIndexAtLeft.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    int data[] = {10, 4, 2, 20, 40, 12, 30};
+    int data_size = sizeof(data)/sizeof(data[0]);
+    int maxAtleft[data_size] = {0};
+    stack<int> stack;
+
+    stack.push(0);
+    maxAtleft[0] = -1;
+
+    for(int i = 1; i < data_size; i++){
+        while(stack.empty()==false && data[stack.top()] <= data[i]){
+            stack.pop();
+        }
+        if(stack.empty())
+            maxAtleft[i] = -1;
+        else
+            maxAtleft[i] = stack.top();
+        stack.push(i);
+    }
+
+    for(int i = 0; i < data_size; i++)
+        cout<< maxAtleft[i]<< " ";
+
+    return 0;
+}
