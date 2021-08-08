f08087d63389df537d0196bcf0357751f503ceb3
Motasim <44056349+motasimmakki@users.noreply.github.com>
2021-08-08 18:39:37 +0530

    Initial commit (Recursive Approach).
    In this problem, we have been given a Binary Tree that can contain -ve as well ad +ve integers and we have to find the Path that contains the maximum sum between any two nodes.
    .
    Time Complexity: O(n), where 'n' is the number of nodes in the tree.
    Auxiliary Space: O(h), where 'n' is the maximum height (Stack space).

---
+++ max_sumPath_between_any_nodes.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+// User Fuction template for C++
+
+class Solution {
+  public:
+    int result = INT_MIN;
+    int Solve(Node* root){
+        if(root == NULL)
+            return 0;
+        int L = Solve(root->left);
+        int R = Solve(root->right);
+        int max_single = max(root->data, max(L+root->data, R+root->data));
+        int max_double = L+root->data+R;
+        result = max(result, max(max_single, max_double));
+        return max_single;
+    }
+    //Function to return maximum path sum from any node in a tree.
+    int findMaxSum(Node* root)
+    {
+        Solve(root);
+        return result;
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        cout << ob.findMaxSum(root) << "\n";
+
+    }
+
+
+    return 0;
+}  // } Driver Code Ends
